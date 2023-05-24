a5f612696d0d90be4661bf1158cc9aba55a8569f
frederico-klein <frekle@kth.se>
2023-05-24 16:19:06 +0200

    added tests, restructured python code, attempt at playing in sync

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-            packages=['moticon_insoles'],
+            packages=['moticon_insoles', 'insoles_common','buffer'],
             package_dir={'':'src'}
         )
 setup(**setup_args)
